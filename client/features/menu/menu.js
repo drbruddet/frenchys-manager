bb86b5b955453cc166b1815b8ee1aa95266c5791
guillaumeko <guillaume.kolly@gmail.com>
2015-11-25 16:21:53 +0100

    change appearence of tables + menu and deconnection link + change functions and trim user email

--- client/features/menu/menu.js
+++ client/features/menu/menu.js
@@ -1,5 +1,16 @@
 Template.menu.events({
 	"click .item": function () {
       	return "active";
-	}
+	},
+	'click #at-nav-button': function(event){
+        event.preventDefault();
+        if (Meteor.userId())
+            AccountsTemplates.logout();
+        else
+            AccountsTemplates.linkClick("signIn");
+    },
   });
+
+Template.menu.rendered = function(){
+  $('.menu .dropdown').dropdown({ });
+}
--- client/features/users/userAdd.js
+++ client/features/users/userAdd.js
@@ -4,13 +4,20 @@ Template.userAdd.events({
 	'submit .new-user': function(event) {
 		event.preventDefault();
 
+
+		var trimInput = function(val) {
+    			return val.replace(/^\s*|\s*$/g, "");
+		};
+		
 		var email = $('input[name="email"]').val();
 		var password = $('input[name="password"]').val();
 		var firstname = $('input[name="firstname"]').val();
 		var lastname = $('input[name="lastname"]').val();
 
-		Meteor.call("createUsers", email, password, firstname, lastname);
+		var emailtrimed = trimInput(email);
 
+		Meteor.call("createUsers", emailtrimed, password, firstname, lastname);
+		
 		event.target.email.value = "";
 		event.target.password.value = "";
 		event.target.firstname.value = "";
